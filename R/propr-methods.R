f98e517bcfce47f75a2c0997b2b3f3719d20c639
Thom Quinn <contacttomquinn@gmail.com>
2016-04-14 19:35:57 -0400

    Added propr.plot method

--- R/propr-methods.R
+++ R/propr-methods.R
@@ -72,3 +72,53 @@ setMethod('$', signature(x = "propr"),
             return(x@pairs[, name])
           }
 )
+
+#' @describeIn propr Method to plot \code{propr} object.
+#'
+# #' @param x An object of class \code{propr}.
+#' @param title A character string. A title for the \code{propr} plot.
+#' @export
+setMethod("plot", signature(x = "propr", y = "missing"),
+          function(x, title = "Pairwise Proportionality"){
+
+            if(!requireNamespace("ggplot2", quietly = TRUE)){
+              stop("Uh oh! This plot method depends on ggplot2! ",
+                   "Try running: install.packages('ggplot2')")
+            }
+
+            if(!requireNamespace("ggthemes", quietly = TRUE)){
+              stop("Uh oh! This plot method depends on ggthemes! ",
+                   "Try running: install.packages('ggthemes')")
+            }
+
+            # Melt *lr counts by feature pairs
+            pairs <- vector("list", nrow(x@pairs))
+            for(i in 1:nrow(x@pairs)){
+
+              cat("Shaping pair", i, "...")
+              pairs[[i]] <- data.frame("x.val" = unlist(x@logratio[x$feature1[i], ]),
+                                       "y.val" = unlist(x@logratio[x$feature2[i], ]),
+                                       "x.id" = x$feature1[i],
+                                       "y.id" = x$feature2[i],
+                                       "group" = i)
+
+              pairs[[i]] <- pairs[[i]][order(pairs[[i]]$x.val), ]
+            }
+
+            # Plot *lr-Y by *lr-X
+            df <- do.call(rbind, pairs)
+            p <- ggplot2::ggplot(data = df, aes(x = x.val,
+                                                y = y.val,
+                                                group = factor(group))) +
+              ggplot2::geom_path(aes(colour = factor(group))) +
+              ggplot2::labs(x = "Exprssion *LR mRNA[1]",
+                            y = "Expression *LR mRNA[2]") +
+              ggplot2::coord_equal(ratio = 1) +
+              ggthemes::theme_base() +
+              ggplot2::theme(legend.position = "none") +
+              ggplot2::ggtitle(title)
+            plot(p)
+
+            return(p)
+          }
+)
