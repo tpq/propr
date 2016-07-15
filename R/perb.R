02da1f3f975bd2b5afcd2a1f56aa3e486e05d890
Thom Quinn <contacttomquinn@gmail.com>
2016-07-15 13:03:43 -0400

    Included Rcpp and removed permutation testing

--- R/perb.R
+++ R/perb.R
@@ -14,7 +14,7 @@
 #' 	columns containing rho measurements for each feature pair. One can think of the resultant
 #' 	matrix as equivalent to a correlation matrix.
 #'
-#' @param ivar A numeric scalar. Specificies feature to use as reference for additive log-ratio transformation.
+#' @param ivar A numeric scalar. Specificies reference feature for additive log-ratio transformation.
 #' @inheritParams phit
 #' @return Returns a \code{propr} object.
 #'
@@ -23,121 +23,19 @@
 #' @examples
 #' randomNum <- sample(1:1000, size = 25 * 10, replace = TRUE)
 #' counts <- matrix(randomNum, nrow = 25, ncol = 10)
-#' prop <- perb(counts, ivar = 0, iter = 0)
+#' prop <- perb(counts, ivar = 0)
 #' @importFrom methods new
 #' @importFrom stats ecdf p.adjust
 #' @export
-perb <- function(counts, ivar = 0, lazyPairs = TRUE, iter = 0, iterSize = ncol(counts) - (ivar > 0), iterHow = 1, onlyDistr = FALSE){
+perb <- function(counts, ivar = 0){
 
-  if(!onlyDistr){
+  cat("Calculating all rho for \"count matrix\"...\n")
+  prop <- new("propr")
+  prop@counts <- as.matrix(counts)
+  if(ivar != 0){ prop@logratio <- alrRcpp(prop@counts, ivar)
+  }else{ prop@logratio <- clrRcpp(prop@counts)}
+  prop@matrix <- rhoRcpp(prop@counts, ivar)
+  prop@pairs <- vector("numeric")
 
-    cat("Calculating all rho for \"count matrix\"...\n")
-    prop <- new("propr")
-    prop@counts <- as.data.frame(counts)
-    if(ivar != 0){ prop@logratio <- as.data.frame(proprALR(prop@counts, ivar))
-    }else{ prop@logratio <- as.data.frame(proprCLR(prop@counts))}
-    prop@matrix <- proprPerb(prop@counts, ivar)
-
-    # Do not populate @pairs slot until after subset
-    if(lazyPairs){
-
-      prop@pairs <- data.frame()
-
-    }else{
-
-      prop@pairs <- proprPairs(prop@matrix)
-      prop@pairs <- prop@pairs[rev(order(abs(prop@pairs$prop))), ]
-      rownames(prop@pairs) <- 1:nrow(prop@pairs)
-    }
-
-    if(iter == 0){
-
-      return(prop)
-    }
-
-  }else{
-
-    if(iter == 0){
-
-      stop("This function cannot return a fit distribution if iter = 0!")
-    }
-  }
-
-  distr <- vector("numeric", iter * iterSize * (iterSize - 1) / 2)
-  for(i in 1:iter){
-
-    cat(paste0("Calculating simulated rho for iter ", i, "...\n"))
-
-    # Handle alr properly
-    if(ivar != 0){
-
-      if(iterHow == 1){
-
-        index.i <- sample(1:ncol(counts[, -ivar]), iterSize)
-        counts.i <- apply(counts[, -ivar][, index.i], 2, sample)
-
-      }else if(iterHow == 2){
-
-
-        counts.i <- t(apply(counts[, -ivar], 1, sample, iterSize))
-
-      }else{
-
-        stop("Uh oh! Provided 'iterHow' not recognized! Select '1' for features and '2' for subject.")
-      }
-
-      fixed <- counts[, ivar]
-      counts.i <- cbind(counts.i, fixed)
-      ivar.i <- ncol(counts.i)
-
-    }else{
-
-      if(iterHow == 1){
-
-        index.i <- sample(1:ncol(counts), iterSize)
-        counts.i <- apply(counts[, index.i], 2, sample)
-
-      }else if(iterHow == 2){
-
-        counts.i <- t(apply(counts, 1, sample, iterSize))
-
-      }else{
-
-        stop("Uh oh! Provided 'iterHow' not recognized! Select '1' for features and '2' for subject.")
-      }
-
-      ivar.i <- 0
-    }
-
-    prop.i <- proprPerb(counts.i, ivar.i)
-    begin <- (i - 1) * iterSize * (iterSize - 1) / 2 + 1
-    end <- (i - 1) * iterSize * (iterSize - 1) / 2 + iterSize * (iterSize - 1) / 2
-    distr[begin:end] <- proprTri(prop.i)
-    rm(prop.i)
-  }
-
-  cat("Fitting rho to distribution...\n")
-  fit <- ecdf(1 - abs(distr))
-  rm(distr)
-
-  if(!onlyDistr){
-
-    cat("Using fit to convert rho into pval...\n")
-    pval <- fit(1 - abs(prop@pairs$prop))
-    # pval[pval >= .5] <- 1 - pval[pval >= .5] # make 2-tails
-    # pval <- pval * 2 # scale 0 to 1
-
-    cat("Correcting for multiple testing...\n")
-    fdr <- p.adjust(pval, method = "BH")
-
-    cat("Building results...\n")
-    prop@pairs <- data.frame(prop@pairs, "pval" = pval, "fdr" = fdr, stringsAsFactors = FALSE)
-    # prop@pairs <- prop@pairs[order(prop@pairs$pval), ]
-
-    return(prop)
-
-  }else{
-
-    return(fit)
-  }
+  return(prop)
 }
--- R/phit.R
+++ R/phit.R
@@ -7,15 +7,10 @@
 #' 	status, treatment status, treatment dose, or time. This function converts a
 #' 	"count matrix" with n rows and d columns into a proportionality matrix of d rows and d
 #' 	columns containing phi measurements for each feature pair. One can think of the resultant
-#' 	matrix as equivalent to a distance matrix, except that it has no symmetry by default.
+#' 	matrix as equivalent to a distance matrix, except that it has no symmetry unless forced.
 #'
 #' @param counts A data.frame or matrix. A "count matrix" with subjects as rows and features as columns.
-#' @param symmetrize A logical. If \code{TRUE}, forces symmetry by duplicating the "lower left triangle".
-#' @param lazyPairs A logical. If \code{TRUE}, \code{@@pairs} slot does not get populated until after subset (to improve performance).
-#' @param iter A numeric scalar. Fits \code{iter*iterSize*(iterSize-1)/2} values to an empiric distribution. Skip with \code{iter = 0}.
-#' @param iterSize A numeric scalar. Fits \code{iter*iterSize*(iterSize-1)/2} values to an empiric distribution.
-#' @param iterHow A numeric scalar. Select \code{1} to randomize feature vectors or \code{2} to randomize subject vectors.
-#' @param onlyDistr A logical. Provided for backend use. Evokes function to return only \code{ecdf} fit.
+#' @param symmetrize A logical. If \code{TRUE}, forces symmetry by reflecting the "lower left triangle".
 #' @return Returns a \code{propr} object.
 #'
 #' @seealso \code{\link{propr}}, \code{\link{propr-class}}, \code{\link{perb}}
@@ -23,95 +18,18 @@
 #' @examples
 #' randomNum <- sample(1:1000, size = 25 * 10, replace = TRUE)
 #' counts <- matrix(randomNum, nrow = 25, ncol = 10)
-#' prop <- phit(counts, symmetrize = TRUE, iter = 0)
+#' prop <- phit(counts, symmetrize = TRUE)
 #' @importFrom methods new
 #' @importFrom stats ecdf p.adjust
 #' @export
-phit <- function(counts, symmetrize = TRUE, lazyPairs = TRUE, iter = 0, iterSize = ncol(counts), iterHow = 1, onlyDistr = FALSE){
-
-  if(lazyPairs & iter > 0){
-
-    stop("This function cannot return a fit distribution if lazily populating @pairs.")
-  }
-
-  if(!onlyDistr){
+phit <- function(counts, symmetrize = TRUE){
 
     cat("Calculating all phi for \"count matrix\"...\n")
     prop <- new("propr")
-    prop@counts <- as.data.frame(counts)
-    prop@logratio <- as.data.frame(proprCLR(prop@counts))
-    prop@matrix <- proprPhit(prop@counts, symmetrize)
-
-    # Do not populate @pairs slot until after subset
-    if(lazyPairs){
-
-      prop@pairs <- data.frame()
-
-    }else{
-
-      prop@pairs <- proprPairs(prop@matrix)
-    }
-
-    if(iter == 0){
-
-      return(prop)
-    }
-
-  }else{
-
-    if(iter == 0){
-
-      stop("This function cannot return a fit distribution if iter = 0!")
-    }
-  }
-
-  distr <- vector("numeric", iter * iterSize * (iterSize - 1) / 2)
-  for(i in 1:iter){
-
-    cat(paste0("Calculating simulated phi for iter ", i, "...\n"))
-
-    if(iterHow == 1){
-
-      index.i <- sample(1:ncol(counts), iterSize)
-      counts.i <- apply(counts[, index.i], 2, sample)
-
-    }else if(iterHow == 2){
-
-      counts.i <- t(apply(counts, 1, sample, iterSize))
-
-    }else{
-
-      stop("Uh oh! Provided 'iterHow' not recognized! Select '1' for features and '2' for subject.")
-    }
-
-    prop.i <- proprPhit(counts.i)
-    begin <- (i - 1) * iterSize * (iterSize - 1) / 2 + 1
-    end <- (i - 1) * iterSize * (iterSize - 1) / 2 + iterSize * (iterSize - 1) / 2
-    distr[begin:end] <- proprTri(prop.i)
-    rm(prop.i)
-  }
-
-  cat("Fitting phi to distribution...\n")
-  fit <- ecdf(distr)
-  rm(distr)
-
-  if(!onlyDistr){
-
-    cat("Using fit to convert phi into pval...\n")
-    pval <- fit(prop@pairs$prop)
-
-    cat("Correcting for multiple testing...\n")
-    fdr <- p.adjust(pval, method = "BH")
-
-    cat("Building results...\n")
-    prop@pairs <- data.frame(prop@pairs, "pval" = pval, "fdr" = fdr, stringsAsFactors = FALSE)
-    prop@pairs <- prop@pairs[order(prop@pairs$pval), ]
-    rownames(prop@pairs) <- 1:nrow(prop@pairs)
+    prop@counts <- as.matrix(counts)
+    prop@logratio <- clrRcpp(prop@counts)
+    prop@matrix <- phiRcpp(prop@counts, symmetrize)
+    prop@pairs <- vector("numeric")
 
     return(prop)
-
-  }else{
-
-    return(fit)
-  }
 }
