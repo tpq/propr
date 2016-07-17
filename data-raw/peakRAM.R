45bf560c10c5e9dcb0f2afea9a69ff5bf23f81ad
Thom Quinn <contacttomquinn@gmail.com>
2016-07-17 11:45:57 -0400

    Modify peakRAM benchmarking script

--- data-raw/peakRAM.R
+++ data-raw/peakRAM.R
@@ -1,7 +1,7 @@
 # Set number of features in counts matrix
 i <- 8000
-X <- t(data.frame("a" = sample(1:i), "b" = sample(1:i), "c" = sample(1:i),
-                  "d" = sample(1:i), "e" = sample(1:i), "f" = sample(1:i)))
+X <- t(data.frame("a" = abs(rnorm(i)), "b" = abs(rnorm(i)), "c" = abs(rnorm(i)),
+                  "d" = abs(rnorm(i)), "e" = abs(rnorm(i)), "f" = abs(rnorm(i))))
 
 # Determine peak RAM needed for phi calculation
 out <- lapply(list(X),
@@ -11,7 +11,7 @@ out <- lapply(list(X),
 
                 for(n in c(10, 100, 500, 1000, 2000, 4000, 6000, 8000)){
 
-                  counts.subset <- counts[, 1:n]
+                  counts.subset <- X[, 1:n]
                   z <- function() {phit(counts.subset)}
                   max <- miSciTools::peakRAM(z)
                   usage <- rbind(usage, data.frame("n" = n, "max" = max))
