29ccd1d6199b5800701e874c312697c3ef4a75c7
Thom Quinn <contacttomquinn@gmail.com>
2016-10-22 17:01:00 +1100

    Add tests to perb including select argument tests

--- tests/testthat/test-perb.R
+++ tests/testthat/test-perb.R
@@ -30,10 +30,115 @@ test_that("calculating rho from phi matches propr:::proprPerb", {
   )
 })
 
+test_that("calculating rho from perb matches propr:::proprPerb", {
+
+  expect_equal(
+    as.vector(perb(X)@matrix),
+    as.vector(propr:::proprPerb(X, ivar = 0))
+  )
+
+  expect_equal(
+    as.vector(perb(X, 3)@matrix[c(1:2, 4:5), c(1:2, 4:5)]),
+    as.vector(propr:::proprPerb(X, ivar = 3))
+  )
+})
+
 test_that("perb accepts ivar name or index", {
 
   expect_equal(
     perb(X, 2),
     perb(X, "b")
+  )
+})
+
+test_that("perb select argument works (clr)", {
+
+  include <- c("b", "d", "e")
+  mat1 <- perb(X, select = include)
+  mat2 <- perb(X)
+  mat2 <- subset(mat2, select = include)
+
+  expect_equal(
+    mat1@counts,
+    mat2@counts
+  )
+
+  expect_equal(
+    mat1@logratio,
+    mat2@logratio
+  )
+
+  expect_equal(
+    mat1@matrix,
+    mat2@matrix
+  )
+})
+
+test_that("perb select argument works (alr) without dropping ivar", {
+
+  include <- c("b", "d", "e")
+  mat1 <- perb(X, 4, select = include)
+  mat2 <- perb(X, 4)
+  mat2 <- subset(mat2, select = include)
+
+  expect_equal(
+    mat1@counts,
+    mat2@counts
+  )
+
+  expect_equal(
+    mat1@logratio,
+    mat2@logratio
+  )
+
+  expect_equal(
+    mat1@matrix,
+    mat2@matrix
+  )
+})
+
+test_that("perb select argument works (alr) with dropping ivar", {
+
+  include <- c("b", "d", "e")
+  mat1 <- perb(X, 3, select = include)
+  mat2 <- perb(X, 3)
+  mat2 <- subset(mat2, select = include)
+
+  expect_equal(
+    mat1@counts,
+    mat2@counts
+  )
+
+  expect_equal(
+    mat1@logratio,
+    mat2@logratio
+  )
+
+  expect_equal(
+    mat1@matrix,
+    mat2@matrix
+  )
+})
+
+test_that("perb select argument works (alr) out of order", {
+
+  include <- c("e", "d", "b")
+  mat1 <- perb(X, 3, select = include)
+  mat2 <- perb(X, 3)
+  mat2 <- subset(mat2, select = include)
+
+  expect_equal(
+    mat1@counts,
+    mat2@counts
+  )
+
+  expect_equal(
+    mat1@logratio,
+    mat2@logratio
+  )
+
+  expect_equal(
+    mat1@matrix,
+    mat2@matrix
   )
 })
