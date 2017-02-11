9e3b11c3b93f088b21fb4afd25a76fd80f012cc8
Thom Quinn <contacttomquinn@gmail.com>
2017-02-12 09:20:02 +1100

    Calculate z and p-val from mean r in aldex.cor

--- R/aldex.cor.R
+++ R/aldex.cor.R
@@ -27,6 +27,7 @@ lr2cor <- function(lr, conditions){
   z <- atanh(r)
   sd <- 1/sqrt(length(conditions) - 3)
   p <- pnorm(abs(z), sd = sd, lower.tail = FALSE)
+
   data.frame(r, z, p)
 }
 
@@ -42,24 +43,33 @@ lr2cor <- function(lr, conditions){
 #'
 #' @return Returns a data.frame of the average
 #'  Pearson's correlation coefficient (\code{r}),
-#'  Fisher's z-transformation (\code{z}), and p-value
-#'  (\code{p}) for each feature across all
-#'  Monte Carlo instances.
+#'  a Fisher's z-transformation (\code{z}) of this
+#'  average, and the associated p-value (\code{p})
+#'  for each feature across all Monte Carlo
+#'  instances.
 #'
 #' @export
 aldex.cor <- function(clr, conditions){
 
+  packageCheck("ALDEx2")
+
   # Keep a running sum of lr2cor instances
   mc <- ALDEx2::getMonteCarloInstances(clr)
   k <- ALDEx2::numMCInstances(clr)
-  out <- 0
+  r <- 0
   for(i in 1:k){
 
     cat(paste0(i, "... "))
     mci_lr <- t(sapply(mc, function(x) x[, i]))
-    out <- out + lr2cor(mci_lr, conditions)
+    r <- r + lr2cor(mci_lr, conditions)$r
   }
   cat("\n")
 
-  return(out / k)
+  r <- r / k
+  z <- atanh(r)
+  sd <- 1/sqrt(length(conditions) - 3)
+  p <- pnorm(abs(z), sd = sd, lower.tail = FALSE)
+
+  data.frame(r, z, p,
+             row.names = colnames(mci_lr))
 }
