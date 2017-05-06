075590fbd9c5a86baf7fd8a155cc4d6fb59c610a
Thom Quinn <contacttomquinn@gmail.com>
2017-05-06 20:41:43 +1000

    Replace data(top) with smaller log-ratio data

--- data-raw/data-pr.R
+++ data-raw/data-pr.R
@@ -15,6 +15,6 @@ keep <- apply(caneToad.counts, 2, function(x) sum(x >= 10) >= 10)
 caneToad.counts <- caneToad.counts[, keep]
 rho <- perb(caneToad.counts)
 best.995 <- rho[">", .995]
-top <- simplify(best.995)
+top <- simplify(best.995)@logratio
 
 devtools::use_data(caneToad.counts, caneToad.groups, top)
