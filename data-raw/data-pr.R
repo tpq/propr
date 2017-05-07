8e927b3d490813409c5f7f6068fef26621d93128
Thom Quinn <contacttomquinn@gmail.com>
2017-05-07 11:14:21 +1000

    Replace top object with top.counts and top.lr

--- data-raw/data-pr.R
+++ data-raw/data-pr.R
@@ -14,7 +14,8 @@ library(propr)
 keep <- apply(caneToad.counts, 2, function(x) sum(x >= 10) >= 10)
 caneToad.counts <- caneToad.counts[, keep]
 rho <- perb(caneToad.counts)
-best.995 <- rho[">", .995]
-top <- simplify(best.995)@logratio
+best.995 <- simplify(rho[">", .995])
+top.counts <- best.995@counts
+top.lr <- best.995@logratio
 
-devtools::use_data(caneToad.counts, caneToad.groups, top)
+devtools::use_data(caneToad.counts, caneToad.groups, top.counts, top.lr)
