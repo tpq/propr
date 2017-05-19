cec4fda7d23fb324d38bf1453f454141c5b90ea9
Thom Quinn <contacttomquinn@gmail.com>
2017-05-20 09:12:42 +1000

    Merge unit tests

--- tests/testthat/test-theta.R
+++ tests/testthat/test-theta.R
@@ -41,3 +41,51 @@ test_that("new FDR matches old FDR", {
     as.vector(propr:::calculateFDR_old(old, oldp))
   )
 })
+
+test_that("boxRcpp matches pseudo-lrv from alphaTheta_old", {
+
+  expect_equal(
+    propr:::boxRcpp(as.matrix(counts), .1),
+    propr:::alphaTheta_old(counts, group, .1)$alrv
+  )
+})
+
+test_that("fast calculateTheta matches slow alphaTheta", {
+
+  expect_equal(
+    as.vector(propr:::calculateTheta(counts, group, .1)$theta),
+    as.vector(propr:::alphaTheta_old(counts, group, .1)$atheta)
+  )
+})
+
+mat <- matrix(sample(0:10, replace = TRUE, size = 150), 10, 15)
+cts <- apply(mat, 2, function(x) sum(x == 0))
+
+df <- data.frame(propr:::labRcpp(ncol(mat)),
+                 "Z" = propr:::ctzRcpp(mat))
+
+test_that("ctzRcpp correctly counts joint zero frequency", {
+
+  for(i in 1:nrow(df)){
+    expect_equal(
+      df$Z[i],
+      cts[df$Partner[i]] + cts[df$Pair[i]]
+    )
+  }
+})
+
+mat <- as.data.frame(matrix(sample(1:10, replace = TRUE, size = 150), 10, 15))
+grp <- c(rep("A", 5), rep("B", 5))
+
+test_that("lrmRcpp correctly calculates log-ratio mean", {
+
+  expect_equal(
+    propr:::calculateTheta_old(mat, grp)$lrm1,
+    propr:::lrmRcpp(as.matrix(mat[1:5, ]))
+  )
+
+  expect_equal(
+    propr:::calculateTheta_old(mat, grp)$lrm2,
+    propr:::lrmRcpp(as.matrix(mat[6:10, ]))
+  )
+})
