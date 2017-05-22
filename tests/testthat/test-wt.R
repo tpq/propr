798922db2619c71659e0270a955dee3d8157cda9
Thom Quinn <contacttomquinn@gmail.com>
2017-05-22 14:42:32 +1000

    Add tests for weighted theta and lrm

--- tests/testthat/test-wt.R
+++ tests/testthat/test-wt.R
@@ -19,3 +19,52 @@ if(requireNamespace("SDMTools", quietly = TRUE)){
     )
   })
 }
+
+data(mail)
+
+test_that("lrm without weights matches lrmRcpp", {
+
+  expect_equal(
+    propr:::lrmRcpp(mail[]),
+    propr:::lrm(mail, mail)
+  )
+})
+
+test_that("lrv without weights matches vlrRcpp", {
+
+  expect_equal(
+    propr:::lltRcpp(propr:::vlrRcpp(mail[])),
+    propr:::lrv(mail, mail)
+  )
+})
+
+if(requireNamespace("limma", quietly = TRUE)){
+
+  data(iris)
+  keep <- iris$Species %in% c("setosa", "versicolor")
+  counts <- iris[keep, 1:4] * 10
+  group <- ifelse(iris[keep, "Species"] == "setosa", "A", "B")
+
+  test_that("calculateTheta matches calculateThetaW_old", {
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$theta,
+      propr::calculateTheta(counts, group, weighted = TRUE)$theta
+    )
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$lrm1,
+      propr::calculateTheta(counts, group, weighted = TRUE)$lrm1
+    )
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$lrm2,
+      propr::calculateTheta(counts, group, weighted = TRUE)$lrm2
+    )
+
+    expect_equal(
+      propr::propd(counts, group, weighted = TRUE)@theta$theta,
+      propr::calculateTheta(counts, group, weighted = TRUE)$theta
+    )
+  })
+}
