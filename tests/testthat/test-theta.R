c4534a248ab32f608e66a16085657ba3f03ad3c8
Thom Quinn <contacttomquinn@gmail.com>
2017-10-06 15:00:04 +1100

    Merge unit tests

--- tests/testthat/test-theta.R
+++ tests/testthat/test-theta.R
@@ -77,15 +77,78 @@ test_that("ctzRcpp correctly counts joint zero frequency", {
 mat <- as.data.frame(matrix(sample(1:10, replace = TRUE, size = 150), 10, 15))
 grp <- c(rep("A", 5), rep("B", 5))
 
-test_that("lrmRcpp correctly calculates log-ratio mean", {
+test_that("lrm correctly calculates log-ratio mean", {
 
   expect_equal(
     propr:::calculateTheta_old(mat, grp)$lrm1,
-    propr:::lrmRcpp(as.matrix(mat[1:5, ]))
+    propr:::lrm(as.matrix(mat[1:5, ]), as.matrix(mat[1:5, ]))
   )
 
   expect_equal(
     propr:::calculateTheta_old(mat, grp)$lrm2,
-    propr:::lrmRcpp(as.matrix(mat[6:10, ]))
+    propr:::lrm(as.matrix(mat[6:10, ]), as.matrix(mat[6:10, ]))
   )
 })
+
+if(requireNamespace("SDMTools", quietly = TRUE)){
+
+  x <- abs(rnorm(20))
+  w <- abs(rnorm(20))
+
+  test_that("weighted mean matches SDMTools", {
+
+    expect_equal(
+      SDMTools::wt.mean(x, w),
+      propr:::wtmRcpp(x, w)
+    )
+  })
+
+  test_that("weighted variance matches SDMTools", {
+
+    expect_equal(
+      SDMTools::wt.var(x, w),
+      propr:::wtvRcpp(x, w)
+    )
+  })
+}
+
+data(mail)
+
+test_that("lrv without weights matches vlrRcpp", {
+
+  expect_equal(
+    propr:::lltRcpp(propr:::vlrRcpp(mail[])),
+    propr:::lrv(mail, mail)
+  )
+})
+
+if(requireNamespace("limma", quietly = TRUE)){
+
+  data(iris)
+  keep <- iris$Species %in% c("setosa", "versicolor")
+  counts <- iris[keep, 1:4] * 10
+  group <- ifelse(iris[keep, "Species"] == "setosa", "A", "B")
+
+  test_that("calculateTheta matches calculateThetaW_old", {
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$theta,
+      propr::calculateTheta(counts, group, weighted = TRUE)$theta
+    )
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$lrm1,
+      propr::calculateTheta(counts, group, weighted = TRUE)$lrm1
+    )
+
+    expect_equal(
+      propr:::calculateThetaW_old(counts, group)$lrm2,
+      propr::calculateTheta(counts, group, weighted = TRUE)$lrm2
+    )
+
+    expect_equal(
+      propr::propd(counts, group, weighted = TRUE)@theta$theta,
+      propr::calculateTheta(counts, group, weighted = TRUE)$theta
+    )
+  })
+}
