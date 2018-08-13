8dd66c0b48e10cea5599a8244867f2dc7933dcf9
Thom Quinn <contacttomquinn@gmail.com>
2018-08-13 10:37:30 +1000

    Add wide2long function for melting count tables

--- R/9-global.R
+++ R/9-global.R
@@ -25,6 +25,31 @@ updateCutoffs <- function(object, cutoff = seq(.05, .95, .3)){
   }
 }
 
+#' Make Long Data from Wide Data
+#'
+#' @param wide A data set in wide format.
+#'
+#' @return A data set in long format.
+#'
+#' @export
+wide2long <- function(wide){
+
+  # Force column names
+  if(is.null(colnames(wide))){
+    colnames(wide) <- paste0("Col", 1:ncol(wide))
+  }
+
+  # Force row names
+  if(is.null(rownames(wide))){
+    rownames(wide) <- as.character(rownames(wide))
+  }
+
+  df <- data.frame("value" = as.vector(as.matrix(wide)))
+  df$variable <- unlist(lapply(colnames(wide), function(x) rep(x, nrow(wide))))
+  df$id <- rownames(wide)
+  return(df)
+}
+
 #' Recast Matrix as Feature Ratios
 #'
 #' The \code{ratios} function recasts a matrix with N feature columns
