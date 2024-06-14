3b5d98551be4dc761064e678558dbae40bc55347
suzannejin <suzanne.jin.yao@gmail.com>
2024-06-14 15:54:21 +0200

    modified getAdj to returned named matrix

--- R/3-shared-getAdj.R
+++ R/3-shared-getAdj.R
@@ -10,14 +10,22 @@
 #'
 #' @export
 getAdj <- function(object, cutoff = 1, above = FALSE){
+
+  # get gene-gene matrix
   m <- getMatrix(object)
+
+  # create adjacency matrix
   a <- matrix(0, nrow(m), ncol(m))
-  a <- matrix(0, nrow(m), ncol(m))
+  rownames(a) <- rownames(m)
+  colnames(a) <- colnames(m)
+
+  # fill in values
   if (above) {
     a[m >= cutoff] <- 1
   } else {
     a[m <= cutoff] <- 1
   }
   diag(a) <- 0
+  
   return(a)
 }
