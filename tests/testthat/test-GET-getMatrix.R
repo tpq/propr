4071466ae9812688c7906776090bb80ab87b1b04
suzannejin <suzanne.jin.yao@gmail.com>
2024-07-02 15:00:20 +0200

    add test for propr too in test-GET-getMatrix

--- tests/testthat/test-GET-getMatrix.R
+++ tests/testthat/test-GET-getMatrix.R
@@ -29,3 +29,22 @@ test_that('get propd matrix work',{
     results[which(results$Partner==3 & results$Pair==1),3]
   )
 })
+
+test_that('get propr matrix work', {
+
+  # create matrix and compute proportionality
+  N <- 100
+  a <- seq(from = 5, to = 15, length.out = N)
+  b <- a * rnorm(N, mean = 1, sd = 0.1)
+  c <- rnorm(N, mean = 10)
+  d <- rnorm(N, mean = 10)
+  e <- rep(10, N)
+  X <- data.frame(a, b, c, d, e)
+  pr <- propr(X, metric = "rho")
+
+  # check
+  expect_equal(
+    pr@matrix,
+    getMatrix(pr)
+  )
+})
