8c720e21c5b30725198037ca79ebb2fd1c813c32
Suzanne Jin <suzanne.jin.yao@gmail.com>
2024-07-24 11:02:20 +0200

    add more tests in test-PROPR-pcor.R

--- tests/testthat/test-PROPR-pcor.R
+++ tests/testthat/test-PROPR-pcor.R
@@ -9,6 +9,53 @@ d <- rnorm(N, mean = 10)
 e <- rep(10, N)
 X <- data.frame(a, b, c, d, e)
 
+test_that("pcor is correct when ivar is clr",{
+  
+    # compute pcor
+    pr <- propr(X, metric = "pcor", ivar="clr")
+  
+    # compute pcor manually
+    ct <- simple_zero_replacement(X)
+    rownames(ct) <- rownames(pr@counts)
+    clr <- t(apply(ct, 1, function(x) log(x) - mean(log(x))))
+    cov <- cov(clr)
+    mat <- corpcor::cor2pcor(cov)
+    class(mat) <- "matrix"
+    colnames(mat)  <- colnames(X)
+    rownames(mat)  <- colnames(X)
+
+    expect_equal(
+      pr@counts,
+      ct
+    )
+  
+    expect_equal(
+      round(pr@matrix, 8),
+      round(mat, 8)
+    )
+  
+  
+})
+
+test_that("pcor is correct when ivar is NA", {
+
+  # compute pcor
+  pr <- propr(X, metric = "pcor", ivar=NA)
+
+  # compute pcor manually
+  cov <- cov(X)
+  mat <- corpcor::cor2pcor(cov)
+  class(mat) <- "matrix"
+  colnames(mat)  <- colnames(X)
+  rownames(mat)  <- colnames(X)
+
+  expect_equal(
+    round(pr@matrix, 8),
+    round(mat, 8)
+  )
+
+})
+
 test_that("pcor with alr and clr are the same", {
 
   # compute pcor with alr
