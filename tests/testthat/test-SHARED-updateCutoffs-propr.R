8cd28d6d2d5a750c77a5383343a03eae035ac75f
suzannejin <suzanne.jin.yao@gmail.com>
2024-09-20 17:08:43 +0200

    add test to check updateCutoffs work in the same way as old code

--- tests/testthat/test-SHARED-updateCutoffs-propr.R
+++ tests/testthat/test-SHARED-updateCutoffs-propr.R
@@ -11,6 +11,154 @@ d <- rnorm(N, mean = 10)
 e <- rep(10, N)
 X <- data.frame(a, b, c, d, e)
 
+# old code
+updateCutoffs_old <- function(object, cutoff, ncores=1){
+
+  getFdrRandcounts <- function(ct.k){
+
+    pr.k <- suppressMessages(
+      propr::propr(ct.k, object@metric, ivar = object@ivar, alpha = object@alpha, p = 0))
+
+    # Vector of propr scores for each pair of taxa.
+    pkt <- pr.k@results$propr
+
+    # Find number of permuted theta less than cutoff
+    sapply(FDR$cutoff, function(cut){
+      if(object@metric == "rho" | object@metric == "cor"){
+        propr:::count_greater_than(pkt, cut)
+      }else{ # phi & phs
+        propr:::count_less_than(pkt, cut)
+      }
+    })
+  }
+
+  if(object@metric == "rho"){
+
+    message("Alert: Estimating FDR for largely positive proportional pairs only.")
+  }
+
+  if(object@metric == "phi"){
+
+    warning("We recommend using the symmetric phi 'phs' for FDR permutation.")
+  }
+
+  if(identical(object@permutes, list(NULL))) stop("Permutation testing is disabled.")
+
+  # Let NA cutoff skip function
+  if(identical(cutoff, NA)) return(object)
+
+  # Set up FDR cutoff table
+  FDR <- as.data.frame(matrix(0, nrow = length(cutoff), ncol = 4))
+  colnames(FDR) <- c("cutoff", "randcounts", "truecounts", "FDR")
+  FDR$cutoff <- cutoff
+  p <- length(object@permutes)
+
+  if(ncores > 1){
+
+    packageCheck("parallel")
+
+    # Set up the cluster and require propr
+    cl <- parallel::makeCluster(ncores)
+    # parallel::clusterEvalQ(cl, requireNamespace(propr, quietly = TRUE))
+
+    # Each element of this list will be a vector whose elements
+    # are the count of theta values less than the cutoff.
+    randcounts <- parallel::parLapply(
+      cl = cl,
+      X = object@permutes,
+      fun = getFdrRandcounts
+    )
+
+    # Sum across cutoff values
+    FDR$randcounts <- apply(as.data.frame(randcounts), 1, sum)
+
+    # Explicitly stop the cluster.
+    parallel::stopCluster(cl)
+
+  }else{
+
+    # Calculate propr for each permutation -- NOTE: `select` and `subset` disable permutation testing
+    for(k in 1:p){
+
+      # Calculate propr exactly based on @metric, @ivar, and @alpha
+      ct.k <- object@permutes[[k]]
+      pr.k <- suppressMessages(
+        propr(ct.k, object@metric, ivar = object@ivar, alpha = object@alpha, p = 0))
+      pkt <- pr.k@results$propr
+
+      # Find number of permuted theta less than cutoff
+      for(cut in 1:nrow(FDR)){ # randcounts as cumsum
+
+        # Count positives as rho > cutoff, cor > cutoff, phi < cutoff, phs < cutoff
+        if(object@metric == "rho" | object@metric == "cor"){
+          FDR[cut, "randcounts"] <- FDR[cut, "randcounts"] + propr:::count_greater_than(pkt, FDR[cut, "cutoff"])
+        }else{ # phi & phs
+          FDR[cut, "randcounts"] <- FDR[cut, "randcounts"] + propr:::count_less_than(pkt, FDR[cut, "cutoff"])
+        }
+      }
+    }
+  }
+
+  # Calculate FDR based on real and permuted tallys
+  FDR$randcounts <- FDR$randcounts / p # randcounts as mean
+
+  for(cut in 1:nrow(FDR)){
+
+    # Count positives as rho > cutoff, cor > cutoff, phi < cutoff, phs < cutoff
+    if(object@metric == "rho" | object@metric == "cor"){
+      FDR[cut, "truecounts"] <- sum(object@results$propr > FDR[cut, "cutoff"])
+    }else{ # phi & phs
+      FDR[cut, "truecounts"] <- sum(object@results$propr < FDR[cut, "cutoff"])
+    }
+
+    FDR[cut, "FDR"] <- FDR[cut, "randcounts"] / FDR[cut, "truecounts"]
+  }
+
+  # Initialize @fdr
+  object@fdr <- FDR
+
+  return(object)
+}
+
+test_that("updateCutoffs_old and updateCutoffs.propr agree - rho", {
+
+  # get propr object and update cutoffs
+  pr <- propr(X, metric = "rho", p=10)
+  set.seed(0)
+  pr <- updateCutoffs(pr, number_of_cutoffs=10)
+  set.seed(0)
+  pr_old <- updateCutoffs_old(pr, cutoff = pr@fdr$cutoff)
+
+  # check that the two methods agree
+  expect_equal(pr@fdr, pr_old@fdr)
+})
+
+test_that("updateCutoffs_old and updateCutoffs.propr agree - phi", {
+
+  # get propr object and update cutoffs
+  pr <- propr(X, metric = "phi", p=10)
+  set.seed(0)
+  pr <- updateCutoffs(pr, number_of_cutoffs=10)
+  set.seed(0)
+  pr_old <- updateCutoffs_old(pr, cutoff = pr@fdr$cutoff)
+
+  # check that the two methods agree
+  expect_equal(pr@fdr, pr_old@fdr)
+})
+
+test_that("updateCutoffs_old and updateCutoffs.propr agree - cor", {
+
+  # get propr object and update cutoffs
+  pr <- propr(X, metric = "cor", p=10)
+  set.seed(0)
+  pr <- updateCutoffs(pr, number_of_cutoffs=10, tails="right")
+  set.seed(0)
+  pr_old <- updateCutoffs_old(pr, cutoff = pr@fdr$cutoff)
+
+  # check that the two methods agree
+  expect_equal(pr@fdr, pr_old@fdr)
+})
+
 test_that("updateCutoffs.propr properly set up cutoffs", {
 
   # get propr object
