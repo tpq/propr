fc0a4b16bf2d7b6f985fa1fd2ad7e43fe332f850
Suzanne Jin <suzanne.jin.yao@gmail.com>
2024-09-26 17:42:05 +0200

    update README with getters. And added more test to getMatrix

--- tests/testthat/test-GET-getMatrix.R
+++ tests/testthat/test-GET-getMatrix.R
@@ -7,26 +7,53 @@ test_that('get propd matrix work',{
   x <- iris[,1:4]  # data matrix with 4 variables
   y <- iris[,5]    # group vector
   pd <- propd(x, as.character(y))
-  mat <- getMatrix(pd)
+  pd <- updateF(pd, moderated=T)
+
+  # get results
+  results <- pd@results
+
+  # get different theta matrices
+  mat_theta <- getMatrix(pd)
+  mat_theta_e <- getMatrix(setActive(pd, what='theta_e'))
+  mat_theta_mod <- getMatrix(setActive(pd, what='theta_mod'))
 
   # check matrix is square and have 4 columns
-  expect_equal(ncol(mat), nrow(mat))
-  expect_equal(ncol(mat), 4)
+  expect_equal(ncol(mat_theta), nrow(mat_theta))
+  expect_equal(ncol(mat_theta), 4)
 
   # check diagonal is zero
-  expect_equal(mat[1,1], 0)
-  expect_equal(mat[2,2], 0)
+  expect_equal(mat_theta[1,1], 0)
+  expect_equal(mat_theta[2,2], 0)
 
-  # check matrix values
-  results <- pd@results
+  # check matrix values are correct
   expect_equal(
-    mat[1,2], 
-    results[which(results$Partner==2 & results$Pair==1),3]
+    mat_theta[1,2], 
+    results[which(results$Partner==2 & results$Pair==1),'theta']
   )
   expect_equal(
-    mat[2,3], 
-    results[which(results$Partner==3 & results$Pair==2),3]
+    mat_theta[2,3], 
+    results[which(results$Partner==3 & results$Pair==2),'theta']
   )
+  expect_equal(
+    mat_theta_e[1,2], 
+    results[which(results$Partner==2 & results$Pair==1),'theta_e']
+  )
+  expect_equal(
+    mat_theta_e[2,3], 
+    results[which(results$Partner==3 & results$Pair==2),'theta_e']
+  )
+  expect_equal(
+    mat_theta_mod[1,2], 
+    results[which(results$Partner==2 & results$Pair==1),'theta_mod']
+  )
+  expect_equal(
+    mat_theta_mod[2,3], 
+    results[which(results$Partner==3 & results$Pair==2),'theta_mod']
+  )
+
+  # check that theta are different from theta_e and theta_mod
+  expect_false(all(mat_theta == mat_theta_e))
+  expect_false(all(mat_theta == mat_theta_mod))
 })
 
 test_that('get propr matrix work', {
