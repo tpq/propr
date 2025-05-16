d8deff10ce13b210029d668e5a64c4bdf6685967
suzannejin <suzanne.jin.yao@gmail.com>
2025-05-16 14:13:23 +0200

    add additional test for lrv

--- tests/testthat/test-SHARED-lrv.R
+++ tests/testthat/test-SHARED-lrv.R
@@ -4,6 +4,7 @@ library(propr)
 # use iris data
 counts <- iris[, 1:4]
 counts <- as.matrix(counts)
+group <- iris[, 5]
 
 test_that("lrv_with_shrinkage and lrv match, when shrinkage is false", {
 
@@ -17,9 +18,27 @@ test_that("lrv_with_shrinkage and lrv match, when shrinkage is false", {
 
 test_that("lrv with_shrinkage and lrv do not match, when shrinkage is true", {
     # calculate lrv with shrinkage
-    lrv1 <- propr:::lrv(counts, counts, TRUE, NA, counts, counts)
+    lrv1 <- propr:::lrv(counts, counts, FALSE, NA, counts, counts)
     lrv2 <- propr:::lrv_with_shrinkage(counts, shrink=TRUE)
 
     # check if they are not equal
     expect_false(identical(lrv1, lrv2))
+})
+
+test_that("lrv_with_shrinkage and lrv match for within group", {
+    # define inputs
+    ct <- as.matrix(counts)
+    w <- ct
+    groups <- lapply(unique(group), function(g) g == group)
+    ngrp <- length(unique(group))
+
+    # calculate lrv for group 1
+    lrv1 <- propr:::lrv(ct[groups[[1]],], w[groups[[1]],], FALSE, NA, ct, w)
+    lrv2 <- propr:::lrv_with_shrinkage(ct[groups[[1]],], shrink=FALSE)
+    expect_equal(lrv1, lrv2)
+
+    # calculate lrv for group 2
+    lrv1 <- propr:::lrv(ct[groups[[2]],], w[groups[[2]],], FALSE, NA, ct, w)
+    lrv2 <- propr:::lrv_with_shrinkage(ct[groups[[2]],], shrink=FALSE)
+    expect_equal(lrv1, lrv2)
 })
