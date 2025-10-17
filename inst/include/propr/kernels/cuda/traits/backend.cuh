04bdc87657246bd607d7de7f67ddf56f75f92f8d
zalbanob <ziyad.albanoby@bsc.com>
2025-10-17 16:05:35 +0200

    more cleanup

---
+++ inst/include/propr/kernels/cuda/traits/backend.cuh
@@ -0,0 +1,26 @@
+namespace propr {
+    namespace cuda {
+            namespace traits {
+            
+            struct common_config {
+                inline static constexpr int BLK_M = 128;
+                inline static constexpr int BLK_K = 8;
+                inline static constexpr int TH_Y  = 8;
+                inline static constexpr int TH_X  = 8;
+            };
+
+            struct cov_config : common_config {};
+            struct cor_config : common_config {};
+            struct lin_config : common_config {};
+            struct vlr_config : common_config {};
+            struct phi_config : common_config {};
+            struct rho_config : common_config {};
+
+            struct sym_config {
+                static constexpr int TILE  = 32; // tile has to be integral multiple of BLK_N
+                static constexpr int BLK_N = 16;
+            };
+
+        }
+    }
+}
